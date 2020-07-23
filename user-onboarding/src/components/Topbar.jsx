67ec6d79224329f1cbb1c94aab29528359a710c9
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-22 20:20:25 -0400

    Set App as material-ui, add Topbar

---
+++ user-onboarding/src/components/Topbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { AppBar,
+         IconButton,
+         Toolbar,
+         Typography } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+
+import EntryDialog from './EntryDialog';
+
+export default function Topbar (props) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+
+  return(
+    <AppBar position="sticky">
+          <Toolbar>
+            <IconButton onClick={() => setIsDialogOpen(true)}>
+              <AddIcon/>
+            </IconButton>
+            <EntryDialog isOpen={isDialogOpen}
+                         setIsOpen={setIsDialogOpen}/>
+            <Typography variant="h6">Team Members</Typography>
+          </Toolbar>
+    </AppBar>
+  );
+}
+
+//  LocalWords:  AddCircleOutlineOutlined EntryDialog
