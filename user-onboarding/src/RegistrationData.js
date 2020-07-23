9ac010e974df2c3feeba373e4d04f2bcdf8c9282
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-23 03:03:46 -0400

    Add RegistrationData class

---
+++ user-onboarding/src/RegistrationData.js
@@ -0,0 +1,8 @@
+export default class RegistrationData {
+  constructor(name, email, password, tosAccepted) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.tosAccepted = tosAccepted;
+  }
+}
