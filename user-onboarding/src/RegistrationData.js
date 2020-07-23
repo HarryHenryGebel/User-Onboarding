4f85e99eafc77b8e9fb6be93099a8b9e0374494c
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-23 03:42:25 -0400

    Make RegistrationData more robust, add registration date

--- user-onboarding/src/RegistrationData.js
+++ user-onboarding/src/RegistrationData.js
@@ -3,6 +3,17 @@ export default class RegistrationData {
     this.name = source.name;
     this.email = source.email;
     this.password = source.password;
-    this.tosAccepted = source.tosAccepted;
+
+    // check if it is from form or response
+    if ('tosAccepted' in source)
+      this.tosAccepted = source.tosAccepted;
+    else
+      this.tosAccepted = `${source.tosChecked}`;
+
+    // only include registration date if in source data
+    if ('createdAt' in source)
+      this.since = Date(source.createdAt);
   }
 }
+
+//  LocalWords:  createdAt tosAccepted
